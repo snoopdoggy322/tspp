baeb7ae198b73588d33faa68286ac0a956b0a8e4
snoopdoggy322 <snoopdoggy322@gmail.com>
2018-05-18 19:06:42 +0300

    Добавленны остальные классы

--- main.cpp
+++ main.cpp
@@ -14,3 +14,49 @@ public:
     string Design;
     string Status;
 };
+class BaseOfArticle {
+public: 
+    int Count;
+    *article Link[];
+    void DeleteArticle(void int);
+    void AddNewArticle(void *article);
+    void EditArticle(void *article);
+};
+
+
+class Editor {
+public: 
+ status CheckStatus(void article);
+    void ChangeStatus(void article);
+    void DeleteArticle(void article);
+    void AddNewArticle(void NameArticle);
+
+};
+
+class journalist: public user {
+public: 
+    void AddContent(void *article);
+    void EditContent(void *article);
+
+};
+class Photographer: public user {
+public: 
+    void AddPhoto(void *article);
+    void CheckCommand(void *Photographer);
+};
+
+
+class Designer {
+public: 
+
+    void AddDesign(void *article);
+    void EditDesign(void *article);
+};
+class Imposer: public user {
+public: 
+    void MakeLayout(void *article[]);
+    void SendToPrint();
+    void ChangeStatus(void articlle);
+};
+
+
