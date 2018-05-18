6fbd595fbd54b2dfa4891d7aae76a5e7e3df567f
snoopdoggy322 <snoopdoggy322@gmail.com>
2018-05-18 18:45:36 +0300

    Добавлен класс Статья

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <conio.h>
+#include <stdio.h>
+#include <string>
+using namespace std;
+class Article {
+public: 
+    string NameArticle;
+    string Content;
+    string Photo;
+    string Design;
+    string Status;
+};
